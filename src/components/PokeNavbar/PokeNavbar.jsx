32b9cc364bfbba3956161e90974081c0fe46b9a1
Facundo Maksud <facundomaksud@gmail.com>
2024-03-29 02:29:15 -0300

    Link de fuentes
    Link para fuente de navbar (Googlefonts)

--- src/components/PokeNavbar/PokeNavbar.jsx
+++ src/components/PokeNavbar/PokeNavbar.jsx
@@ -4,30 +4,80 @@ import iconPokeball from "../../assets/Icons/pokeball_pokemon_icon.png";
 import iconPlayer from "../../assets/Icons/play_pokemon_icon.png";
 import iconNoticias from "../../assets/Icons/pokedex_pokemon_icon.png";
 import styles from "./PokeNavbar.module.css";
+import { useState } from "react";
 
 export const PokeNavbar = () => {
+  const [click, setClick] = useState("Inicio");
+  const optionClick = (item) => {
+    setClick(item);
+  };
   return (
     <header className={styles.headerContainer}>
       <nav className={styles.navContainer}>
-        <div>
+        <div
+          className={
+            click === "Inicio"
+              ? `${styles.navOptions} ${styles.optionInicio} ${styles.isActiveInicio}`
+              : `${styles.navOptions} ${styles.optionInicio}`
+          }
+          onClick={() => {
+            optionClick("Inicio");
+          }}
+        >
+          <img src={iconPikachu} alt="pikachu-icon" />
           <p>Inicio</p>
-          <img src={iconPikachu} alt="pikachu-icon" width={40} />
         </div>
-        <div>
+        <div
+          className={
+            click === "Pokedex"
+              ? `${styles.navOptions} ${styles.optionPokedex} ${styles.isActivePokedex}`
+              : `${styles.navOptions} ${styles.optionPokedex}`
+          }
+          onClick={() => {
+            optionClick("Pokedex");
+          }}
+        >
+          <img src={iconPokeball} alt="pokeball-icon" />
           <p>Pokédex</p>
-          <img src={iconPokeball} alt="pokeball-icon" width={40} />
         </div>
-        <div>
+        <div
+          className={
+            click === "Aplicaciones"
+              ? `${styles.navOptions} ${styles.optionAplicaciones} ${styles.isActiveAplicaciones}`
+              : `${styles.navOptions} ${styles.optionAplicaciones}`
+          }
+          onClick={() => {
+            optionClick("Aplicaciones");
+          }}
+        >
+          <img src={iconAplicaciones} alt="phone-icon" />
           <p>Aplicaciones</p>
-          <img src={iconAplicaciones} alt="phone-icon" width={40} />
         </div>
-        <div>
+        <div
+          className={
+            click === "Eventos"
+              ? `${styles.navOptions} ${styles.optionEventos} ${styles.isActiveEventos}`
+              : `${styles.navOptions} ${styles.optionEventos}`
+          }
+          onClick={() => {
+            optionClick("Eventos");
+          }}
+        >
+          <img src={iconPlayer} alt="play-icon"></img>
           <p>Eventos</p>
-          <img src={iconPlayer} alt="play-icon" width={40}></img>
         </div>
-        <div>
+        <div
+          className={
+            click === "Noticias"
+              ? `${styles.navOptions} ${styles.optionNoticias} ${styles.isActiveNoticias}`
+              : `${styles.navOptions} ${styles.optionNoticias}`
+          }
+          onClick={() => {
+            optionClick("Noticias");
+          }}
+        >
+          <img src={iconNoticias} alt="pokedex-icon" />
           <p>Noticias</p>
-          <img src={iconNoticias} alt="pokedex-icon" width={40} />
         </div>
       </nav>
     </header>
