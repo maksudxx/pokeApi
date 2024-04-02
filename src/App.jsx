7f8385eb6b5e7ac6580cb0e095890ae447ac63c3
Facundo Maksud <facundomaksud@gmail.com>
2024-04-02 02:12:43 -0300

    Refactor etiquetas

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { PokeNavbar } from "./components/PokeNavbar/PokeNavbar";
 import styles from "./App.module.css";
 export const App = () => {
   return (
-    <>
+    <main>
       <PokeNavbar />
       <div className={styles.containerRoot}>
         <div className={styles.containerWhite}>
           <Inicio />
         </div>
       </div>
-    </>
+    </main>
   );
 };
--- src/Views/Inicio/Inicio.jsx
+++ src/Views/Inicio/Inicio.jsx
@@ -2,7 +2,9 @@ import styles from './Inicio.module.css'
 export const Inicio = () => {
   return (
     <div className={styles.containerInicio}>
-      <h1>POKEMONES</h1>
+      <div>
+        
+      </div>
     </div>
   );
 };
