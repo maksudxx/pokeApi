09740bb988bfb40ffe6622471125c4b1d9447fda
Facundo Maksud <facundomaksud@gmail.com>
2024-04-02 02:41:52 -0300

    Componente inicio creado y estilos css

--- src/Views/Inicio/Inicio.jsx
+++ src/Views/Inicio/Inicio.jsx
@@ -1,10 +1,15 @@
 import styles from './Inicio.module.css'
+import imgInicio from '../../assets/Images/Imagen-Inicio.png'
 export const Inicio = () => {
   return (
-    <div className={styles.containerInicio}>
-      <div>
-        
-      </div>
-    </div>
+    <article className={styles.containerInicio}>
+      <section className={styles.cardInicio}>
+        <img src={imgInicio} alt="Imagen-Inicio" />
+        <div className={styles.textCardInicio}>
+          <h3>Nuevos Pokémon Paradoja distorsionan el espacio-tiempo en Escarlata y Púrpura-Fuerzas Temporales</h3>
+          <p>Descubre nuevos Pokémon del pasado y del futuro, el regreso de las poderosas cartas de AS TÁCTICO y mucho más en la proxima expansión de JCC Pokémon.</p>
+        </div>
+      </section>
+    </article>
   );
 };
