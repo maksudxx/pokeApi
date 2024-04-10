0e51ceb14b5aa77f2b63654ea120e7bf77dd8f19
Facundo Maksud <facundomaksud@gmail.com>
2024-04-10 00:28:49 -0300

    View Pokedex agregada

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { Inicio } from "./Views/Inicio/Inicio";
 import { PokeNavbar } from "./components/PokeNavbar/PokeNavbar";
 import { Footer } from "./components/Footer/Footer";
 import styles from "./App.module.css";
+import { Pokedex } from "./Views/Pokedex/Pokedex";
 export const App = () => {
   return (
     <main>
       <PokeNavbar />
       <div className={styles.containerRoot}>
         <div className={styles.containerWhite}>
-          <Inicio />
+          {/* <Inicio /> */}
+          <Pokedex />
         </div>
       </div>
       <Footer />
