8be72240adb0a0272123edbee703c2c884a4ad71
Facundo Maksud <facundomaksud@gmail.com>
2024-04-16 03:57:50 -0300

    grid pokemon

--- src/Views/Pokedex/Pokedex.jsx
+++ src/Views/Pokedex/Pokedex.jsx
@@ -1,15 +1,16 @@
 import { SearchBar } from "../../components/SearchBar/SearchBar";
 import styles from "./Pokedex.module.css";
 import { useFetchPokemon } from "../../hooks/useFetchPokemon";
+import { GridPokemon } from "../../components/GridPokemon/GridPokemon";
 
 export const Pokedex = () => {
   const { pokemon, isLoading } = useFetchPokemon();
-  console.log(pokemon);
+  
 
   return (
     <article className={styles.containerPokedex}>
       <SearchBar />
-      {isLoading ? <h1>Cargando</h1> : null}
+      {isLoading ? <h1>Cargando</h1> : <GridPokemon pokemon={pokemon}/>}
     </article>
   );
 };
