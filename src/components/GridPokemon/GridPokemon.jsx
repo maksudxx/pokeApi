996b0d211f692487802b0add9464eefa78c04227
Facundo Maksud <facundomaksud@gmail.com>
2024-04-17 02:46:03 -0300

    estilos css

--- src/components/GridPokemon/GridPokemon.jsx
+++ src/components/GridPokemon/GridPokemon.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react";
 import { PokeCard } from "../PokeCard/PokeCard";
 import styles from "./GridPokemon.module.css";
 
 export const GridPokemon = ({ pokemon }) => {
-  console.log(pokemon);
+  const [startIndex, setStartIndex] = useState(0);
+  const handleNextClick = () => {
+    setStartIndex((prev) => prev + 30);
+  };
+
+  //dividimos los pokemon de 30 en 30 para poder mostrarlos por partes
+  const pokemonToShow = pokemon.slice(0, startIndex + 30);
+
   return (
     <div className={styles.gridContainer}>
-      {pokemon?.map(({ id, name, height, sprites, stats, weight, types }) => (
-        <PokeCard
-          id={id}
-          name={name}
-          height={height}
-          sprites={sprites}
-          stats={stats}
-          weight={weight}
-          types={types}
-        />
-      ))}
+      {pokemonToShow?.map(
+        ({ id, name, height, sprites, stats, weight, types }) => (
+          <PokeCard
+            id={id}
+            name={name}
+            height={height}
+            sprites={sprites}
+            stats={stats}
+            weight={weight}
+            types={types}
+          />
+        )
+      )}
+      <div></div>
+      <button onClick={handleNextClick} className={styles.gridButtonPokemon}>Ver mas...</button>
     </div>
   );
 };
