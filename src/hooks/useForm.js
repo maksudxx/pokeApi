bd3d72fe18ba6ff98a9d429fb39f1e15512ef9ca
Facundo Maksud <facundomaksud@gmail.com>
2024-05-19 18:14:47 -0300

    Hook useForm para formulario de footer

---
+++ src/hooks/useForm.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export const useForm = (initialForm = {}) => {
+  const [formState, setFormState] = useState(initialForm);
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    console.log(value);
+    setFormState({ ...formState, [name]: value });
+  };
+  return { formState, onInputChange };
+};
